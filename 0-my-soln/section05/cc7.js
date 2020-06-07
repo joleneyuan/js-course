5aedebfeecc4d09a5d5e31d8963f02629b360655
Jolene Yuan <joyuan106@gmail.com>
2020-06-06 20:46:33 -0400

    sect 5

--- 0-my-soln/section05/cc7.js
+++ 0-my-soln/section05/cc7.js
@@ -68,4 +68,71 @@ var personProto = {
     }
 }
 
-var jane = Object.create(personProto);
+var jane = Object.create(personProto, {
+    name: { value: 'jane'},
+    birthYear: { value: 1989 },
+    job: { value: 'developer' }
+}); 
+
+/*
+Difference between Object.create and Function Constructor is 
+that Object.create inherits directly from the one that we passed
+into the first argument, whereas a newly created object using
+Function Constructor inherits from the constructor's prototype
+property.
+
+john -> Person {name: "john", birthYear: 1990, job: "teacher"}
+jane -> {name: "jane", birthYear: 1989, job: "developer"}
+*/
+
+//==============================================================
+
+// Primitives vs objects
+
+/*
+Primitives -> hold data within that Object
+Object -> reference in memory where the data is held
+*/
+
+var a = 23;
+var b = a;
+a = 46;
+console.log('a: ' + a); // 46
+console.log('b: ' + b); // 23
+
+var c = {
+    name: 'john',
+    age: 26
+};
+var d = c;
+c.name = 'jane';
+console.log('c: ' + c.name); // jane
+console.log('d: ' + d.name); // jane
+
+// functions
+
+var age = 27;
+var jonas = {
+    name: 'jonas',
+    city: 'lisbon'
+}
+
+function change(a,b){
+    a = 30;
+    b.city = 'toronto'
+}
+
+change(age, jonas);
+console.log(age); // 27
+console.log(jonas); // {name: "jonas", city: "toronto"}
+age = 30;
+console.log(age); // 30
+
+
+var e = [1,2,3];
+var f = e;
+console.log('e: ' + e);
+console.log('f: ' + f);
+e[0] = 5;
+console.log('e: ' + e);
+console.log('f: ' + f);
